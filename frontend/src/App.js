1669c42c4d2394448c57fcc8963034a3b3444c68
Mihail <borisonchick@gmail.com>
2022-03-07 19:18:03 +0300

    Add block Menu, UserList and Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
+import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import UserList from "./components/Users";
+import Footer from "./components/Footer";
+import Menu from "./components/Menu";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'users': []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8000/api/users/').then(response => {
+                this.setState(
+                    {
+                        "users": response.data
+                    }
+                )
+            }
+        ).catch(error => console.log(error))
+    }
+
+    render() {
+        return (
+            <div>
+                <Menu />
+                <UserList users={this.state.users}/>
+                <Footer/>
+            </div>
+        );
+    }
 }
 
 export default App;
