2e2a2b73e20215a6a4c91b3b33cf945ecc0b6a7e
Mihail <borisonchick@gmail.com>
2022-03-28 10:12:45 +0300

    Add pagintation, page size, filters. Set up views.

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "corsheaders",
     "usersapp",
     "todoapp",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
@@ -144,4 +145,10 @@ REST_FRAMEWORK = {
         'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
         'djangorestframework_camel_case.parser.CamelCaseJSONParser',
     ),
+
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
+
 }
--- usersapp/views.py
+++ usersapp/views.py
@@ -1,10 +1,10 @@
-from rest_framework.renderers import StaticHTMLRenderer
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.viewsets import GenericViewSet
 
 from .models import User
 from .serializers import UserModelSerializer
 
 
-class UserModelViewSet(ModelViewSet):
+class UserCustomViewSet(ListModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
