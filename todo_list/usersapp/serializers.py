70e03e84fd0074d6e262d129a12351d2b63ab7b0
Mihail <borisonchick@gmail.com>
2022-11-15 11:34:41 +0300

    Fix bug

---
+++ todo_list/usersapp/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import User
+
+
+class UserBaseModelSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+        )
+
+
+class UserModelSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "is_superuser",
+            "is_staff",
+        )
